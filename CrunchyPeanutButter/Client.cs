a4563f45fb79598d0f14fc22a7fb241f66c73011
Matt Heiler <heiler.matthewd@gmail.com>
2021-04-17 13:56:15 -0500

    Update Client.cs

--- CrunchyPeanutButter/Client.cs
+++ CrunchyPeanutButter/Client.cs
@@ -1,10 +1,24 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 
 namespace CrunchyPeanutButter
 {
     public partial class Client
     {
         partial void UpdateJsonSerializerSettings(JsonSerializerOptions settings)
+        {
+        }
+
+        partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+        {
+        }
+
+        partial void PrepareRequest(HttpClient client, HttpRequestMessage request, StringBuilder urlBuilder)
+        {
+        }
+
+        partial void ProcessResponse(HttpClient client, HttpResponseMessage response)
         {
         }
     }
