b5f40b0025120b208fb7b4b8d85de89ef19e16d0
Matt Heiler <mheiler@questarai.com>
2021-06-16 07:34:26 -0500

    Added other example behaviors.

--- CrunchyPeanutButter.Application/Setup.cs
+++ CrunchyPeanutButter.Application/Setup.cs
@@ -12,7 +12,9 @@ namespace CrunchyPeanutButter.Application
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestLoggingBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
             AssemblyScanner.FindValidatorsInAssembly(Assembly.GetExecutingAssembly()).ForEach(item => services.AddScoped(item.InterfaceType, item.ValidatorType));
             return services;
         }
